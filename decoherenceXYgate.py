2bc14bd4753bd3f483e31ed1606673be9cac73cb
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-10-31 00:00:10 +0400

    Update decoherenceXYgate.py

--- decoherenceXYgate.py
+++ decoherenceXYgate.py
@@ -1,27 +1,51 @@
-import os
-from PIL import Image
-import imageio
+from cmath import pi
+import matplotlib.pyplot as plt
+import numpy as np
+from qutip import *
 
-def create_gif(input_folder, output_gif):
-    # Get a list of all image files in the folder
-    image_files = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
-    image_files = [f for f in image_files if f.lower().endswith(('.png'))]
 
-    # Sort the images to maintain order
-    image_files.sort()
+def applyGate(b, gate, initial_state, points):
+    times = np.linspace(0, pi, 21)
+    states = []
 
-    # Open and convert each image to RGB mode
-    images = []
-    for image_file in image_files:
-        with Image.open(os.path.join(input_folder, image_file)) as img:
-            img = img.convert('RGBA')
-            images.append(img)
+    gamma = 0.3
+    c_ops = [np.sqrt(gamma) * destroy(2)]
+    states = []
+    i=0
+    for t in times:
+        result = mesolve(gate, initial_state, [0, 0.5*t], c_ops)
+        states.append(result.states[-1])
 
-    # Save as GIF using imageio
-    imageio.mimsave(output_gif, images, format='GIF', duration=0.5)  # Adjust duration as needed
+        b.add_states(points, kind='point')
+        b.add_states(states[i])
+        b.save()
+        points.append(states[i])
+        b.clear()
+        i+=1
+    
+    return b, states[-1], points
 
-# Usage
-input_folder = "C:/Users/dk4770/Downloads/a"
-output_gif = "Zgate.gif"
+initial_state = basis(2, 0)
+y_gate = sigmay()
+hadamard_gate = qutip.qip.operations.hadamard_transform()
+z_gate = sigmaz()
+x_gate = sigmax()
 
-create_gif(input_folder, output_gif)
+b = Bloch()
+b.vector_color = ['b']  # Set vector color to blue
+b.point_color = ['r']
+b.point_marker = ['o']
+b.point_size = [25]
+points = []
+
+#points.append(initial_state)
+b.view = [180,30]
+
+b, initial_state, points= applyGate(b, x_gate, initial_state, points)
+
+b, initial_state, points= applyGate(b, y_gate, initial_state, points)
+
+b.add_states(points, kind='point')
+b.add_states(points[-1])
+b.show()
+plt.show()
