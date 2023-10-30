0fa5e561abea4b876c4c83da954dcc9eea9917eb
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-10-30 20:53:28 +0400

    Update Ygete.py

--- Ygete.py
+++ Ygete.py
@@ -1,26 +1,36 @@
+from cmath import pi
 import matplotlib.pyplot as plt
-from qutip import *
 import numpy as np
+from qutip import *
 
 # Define the initial state (for example, |0>)
 initial_state = basis(2, 0)
 
-# Define the X gate transformation
-x_gate = sigmax()
 
 # Define the times at which to observe the state (0 to 1 in 100 steps)
-times = np.linspace(0, 1, 100)
+times = np.linspace(0, pi, 21)
 
 # Initialize an empty list to store states
 states = []
-
-# Evolve the initial state under the X gate at different time steps
-for t in times:
-    state_t = propagator(x_gate, t, [], True) * initial_state
-    states.append(state_t)
-
-# Visualize the states on the Bloch sphere
+points = []
 b = Bloch()
-b.add_states(states)
+b.vector_color = ['b']  # Set vector color to blue
+b.point_color = ['r']
+b.point_marker = ['o']
+point_size = [25]
+y_gate = sigmay()
+
+# Evolve the initial state under the Hadamard gate at different time steps
+for t in times:
+    b.add_states(points, kind='point')
+    b.add_states(((1j * 0.5*t * y_gate).expm()) * initial_state)
+    b.save()
+    points.append(((1j * 0.5*t * y_gate).expm()) * initial_state)
+    b.clear()
+# Visualize the states on the Bloch sphere
+b.add_states(states)  # Add states as points
+b.make_sphere()  # Draw the Bloch sphere
+
+# Show the Bloch sphere
 b.show()
 plt.show()
